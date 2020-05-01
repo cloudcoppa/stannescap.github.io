1264e837cabf7c682556c4b0b7b88ee8b4983231
cloudcoppa <64659488+cloudcoppa@users.noreply.github.com>
2020-05-01 14:36:14 -0700

    removed

--- annes/script.js
+++
@@ -1,9 +0,0 @@
-var database * [
-{
-	username: "cloud",
-	password: "yungprezi"
-}
-];
-
-var userNamePrompt * prompt ("what's your username?");
-var paswordPrompt * prompt ("what's your password?");
