44ef14d15012c711cbe08a3a3826f4e86329aeda
cloudcoppa <64659488+cloudcoppa@users.noreply.github.com>
2020-05-01 14:23:10 -0700

    added new files

---
+++ annes/database.js
@@ -0,0 +1,9 @@
+var database * [
+{
+	username: "cloud",
+	password: "yungprezi"
+}
+];
+
+var userNamePrompt * prompt ("what's your username?");
+var paswordPrompt * prompt ("what's your password?");
